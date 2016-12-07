e2079503a70adc9c5c15bc7bfef380c66a0141a7
Andrea <andrea.zhang@go.edgehill.ac.uk>
2016-12-07 20:51:30 +0000

    Board class update

--- src/main/java/bot/Board.java
+++ src/main/java/bot/Board.java
@@ -2,17 +2,22 @@ package bot;
 
 import java.util.Random;
 
+import org.json.JSONObject;
+
 public class Board {
 	
 	private final Api api;
+	private final String password;
 
 	public int[][] matrix = new int[6][7];
 	
-	public Board(Api api){
+	public Board(Api api, String password){
 		this.api = api;
+		this.password = password;
+		
 	}
 	
-	public void addPiece(){
+	public void addPiece(JSONObject game, String playerId){
 		
 		//Generate random column to add piece
 		Random random = new Random();
